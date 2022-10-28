d20d8ee4e7d3a34b5fc5f51995cb96714e8ba201
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-28 12:58:10 +0300

    backend: added location relation in post model

--- touristy-backend/app/Models/Post.php
+++ touristy-backend/app/Models/Post.php
@@ -22,4 +22,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
 }
