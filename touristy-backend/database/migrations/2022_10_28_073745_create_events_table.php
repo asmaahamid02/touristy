5a028f9c82372113ad65b7ffa28721e7be0c8cf7
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-28 10:40:02 +0300

    backend: added events migration

---
+++ touristy-backend/database/migrations/2022_10_28_073745_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('location_id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
