f1216ad80f3a51ec8b213a202e1840fb85c9ec9c
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-29 00:31:11 +0300

    backend: added posts media migration

---
+++ touristy-backend/database/migrations/2022_10_28_212244_create_posts_media_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts_media', function (Blueprint $table) {
+            $table->id();
+            $table->string('media_path');
+            $table->integer('post_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts_media');
+    }
+}
