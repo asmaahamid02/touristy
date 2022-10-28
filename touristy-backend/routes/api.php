e9013a65edd865acecf8428376d9ef1ad7db4060
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-28 20:28:27 +0300

    backend: added auth me api route

--- touristy-backend/routes/api.php
+++ touristy-backend/routes/api.php
@@ -9,4 +9,8 @@ Route::group(['prefix' => 'v0.1'], function () {
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
     });
+
+    Route::group(['middleware' => ['jwt.verify']], function () {
+        Route::get('/me', [AuthController::class, 'me']);
+    });
 });
