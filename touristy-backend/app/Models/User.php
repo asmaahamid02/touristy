8a6d5d8c46928af2cb103a3df9165fbdd798aa6d
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-28 17:12:47 +0300

    backend: implemented JWTSubject in user model

--- touristy-backend/app/Models/User.php
+++ touristy-backend/app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
     protected $fillable = [
         'first_name',
         'last_name',
@@ -29,6 +29,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // JWTSubject interface methods
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     // Relationships
     public function nationality()
