7e2bb183104babf078e23beda307c2369df002af
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-28 13:21:30 +0300

    backend: added groups relation in user model

--- touristy-backend/app/Models/User.php
+++ touristy-backend/app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Trip::class);
     }
+
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
