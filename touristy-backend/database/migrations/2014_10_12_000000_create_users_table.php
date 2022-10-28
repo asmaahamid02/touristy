7754140ea6eb7b0f46cf9c748755be9ffd5de966
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-28 08:21:45 +0300

    backend: updated users migration

--- touristy-backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ touristy-backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->string('profile_picture')->nullable();
+            $table->string('cover_picture')->nullable();
+            $table->string('ip_address');
             $table->rememberToken();
             $table->timestamps();
         });
