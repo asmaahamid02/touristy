82051cf665c927ea6650f5031698e0c71cfa5904
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-29 14:52:57 +0300

    backend: added create function in trip controller

--- touristy-backend/app/Http/Controllers/TripController.php
+++ touristy-backend/app/Http/Controllers/TripController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Trip;
+use App\Traits\LocationTrait;
 use App\Traits\ResponseJson;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class TripController extends Controller
 {
-    use ResponseJson;
+    use ResponseJson, LocationTrait;
     public function index()
     {
         $trips = Trip::orderBy('created_at', 'desc')->with('user')->with('location')->get();
@@ -21,9 +24,43 @@ class TripController extends Controller
         return $this->jsonResponse($trips, 'data', Response::HTTP_OK, 'Trips');
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'arrival_date' => 'required_without:is_past|date',
+            'departure_date' => 'required_without:is_past|date',
+            'is_past' => 'required_without_all:arrival_date,departure_date|date',
+            'longitude' => 'required|numeric',
+            'latitude' => 'required|numeric',
+            'city' => 'required|string',
+            'country' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->jsonResponse($validator->errors(), 'data', Response::HTTP_BAD_REQUEST, 'Validation error');
+        }
+
+        $location_id = $this->saveLocation($request->latitude, $request->longitude, $request->city, $request->country);
+
+        $trip = new Trip();
+
+        $trip->user_id = Auth::id();
+        $trip->location_id = $location_id;
+        $trip->title = $request->title;
+        $trip->description = $request->description;
+
+        if ($request->has('is_past')) {
+            $trip->is_past = $request->is_past;
+        } else {
+            $trip->arrival_date = $request->arrival_date;
+            $trip->departure_date = $request->departure_date;
+        }
+
+        $trip->save();
+
+        return $this->jsonResponse($trip->load(['location']), 'data', Response::HTTP_CREATED, 'Trip created');
     }
 
     public function show($id)
