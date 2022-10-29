55684a0e8aa421d2854214c6325187e2de41aaf1
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-29 11:05:53 +0300

    backend: modified posts relation arguments

--- touristy-backend/app/Models/Tag.php
+++ touristy-backend/app/Models/Tag.php
@@ -15,6 +15,6 @@ class Tag extends Model
 
     public function posts()
     {
-        return $this->belongsToMany(Post::class);
+        return $this->belongsToMany(Post::class, 'posts_tags', 'tag_id', 'post_id')->withTimestamps();
     }
 }
