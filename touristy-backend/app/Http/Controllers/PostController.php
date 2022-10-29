fdad6099b182513d4677ba23048ff3f12f9d0dcb
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-29 11:07:34 +0300

    backend: completed create function in post controller

--- touristy-backend/app/Http/Controllers/PostController.php
+++ touristy-backend/app/Http/Controllers/PostController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Tag;
+use App\Traits\MediaTrait;
 use App\Traits\ResponseJson;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
-    use ResponseJson;
+    use ResponseJson, MediaTrait;
     public function index()
     {
         $posts  = Post::where('is_deleted', 0)->get();
@@ -24,31 +28,32 @@ class PostController extends Controller
 
     public function create(Request $request)
     {
+        //validate data
         $validator = Validator::make($request->all(), [
             'content' => 'required_without:media|string',
             'tags' => 'nullable|array',
             'tags.*' => 'string',
-            'media' => ['required_without:content', function ($attribute, $value, $fail) {
-                if (count($value) > 10) {
-                    $fail('You can only upload 10 media files at a time');
-                }
-            }],
+            'media' => 'required_without:content|between:1,10|array',
             'media.*' => [
                 function ($attribute, $value, $fail) {
+                    //check if is it image
                     $is_image = Validator::make(
                         ['upload' => $value],
                         ['upload' => 'image|mimes:jpeg,png,jpg']
                     )->passes();
 
+                    //check if is it video
                     $is_video = Validator::make(
                         ['upload' => $value],
                         ['upload' => 'mimetypes:video/avi,video/mpeg,video/quicktime']
                     )->passes();
 
+                    //return error if not image or video
                     if (!$is_video && !$is_image) {
                         $fail(':attribute must be image (.png, .jpeg, .jpg) or video.');
                     }
 
+                    //if video, check if it is less than 10MB, less than 60 sec minutes
                     if ($is_video) {
                         $validator = Validator::make(
                             ['video' => $value],
@@ -59,6 +64,7 @@ class PostController extends Controller
                         }
                     }
 
+                    //if image, check if it is less than 2MB
                     if ($is_image) {
                         $validator = Validator::make(
                             ['image' => $value],
@@ -73,8 +79,55 @@ class PostController extends Controller
             'publicity' => 'required|string|in:public,followers',
         ]);
 
+        //return error if validation fails
         if ($validator->fails()) {
             return $this->jsonResponse($validator->errors(), 'errors', Response::HTTP_BAD_REQUEST, 'Validation error');
+        }
+
+        try {
+            //create post
+            $post = new Post();
+            $user_id = Auth::id();
+            $post->user_id = $user_id;
+
+            if ($request->has('content')) {
+                $post->content = $request->content;
+            }
+
+            $post->publicity = $request->publicity;
+
+            $post->save();
+
+            //save tags
+            if ($request->has('tags')) {
+                foreach ($request->tags as $tag) {
+
+                    Tag::firstOrCreate(['tag' => $tag])->posts()->attach($post->id);
+                }
+            }
+
+            //save media
+            if ($request->has('media')) {
+                foreach ($request->media as $mediaFile) {
+                    $mime = $this->getMimeType($mediaFile);
+
+                    if ($mime == 'image') {
+                        $post->media()->create([
+                            'media_type' => 'image',
+                            'media_path' => $this->uploadMedia($mediaFile, 'posts/images/' . $user_id . '/' . $post->id),
+                        ]);
+                    } else {
+                        $post->media()->create([
+                            'media_type' => 'video',
+                            'media_path' => $this->uploadMedia($mediaFile, 'posts/videos/' . $user_id . '/' . $post->id),
+                        ]);
+                    }
+                }
+            }
+            return $this->jsonResponse($post->load(['tags', 'media']), 'data', Response::HTTP_CREATED, 'Post created');
+        } catch (Exception $error) {
+
+            return $this->jsonResponse($error->getMessage(), 'data', Response::HTTP_INTERNAL_SERVER_ERROR, 'Internal server error');
         }
     }
 
@@ -101,5 +154,15 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function getMimeType($file)
+    {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $file);
+        $mime = explode('/', $mime)[0];
+        finfo_close($finfo);
+
+        return $mime;
     }
 }
