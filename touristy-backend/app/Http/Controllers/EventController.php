a50437b105894ce0e13fe571bf69b375698534bd
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-29 20:31:55 +0300

    backend: added create event function

--- touristy-backend/app/Http/Controllers/EventController.php
+++ touristy-backend/app/Http/Controllers/EventController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Traits\LocationTrait;
 use App\Traits\MediaTrait;
 use App\Traits\ResponseJson;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class EventController extends Controller
 {
-    use ResponseJson, MediaTrait;
+    use ResponseJson, MediaTrait, LocationTrait;
     public function index()
     {
         $events = Event::orderBy('created_at', 'desc')->get();
@@ -22,9 +26,42 @@ class EventController extends Controller
         return $this->jsonResponse($events, 'data', Response::HTTP_OK, 'Events');
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'start_date' => 'required|date|after_or_equal:now',
+            'end_date' => 'date|after_or_equal:start_date',
+            'image' => 'base64image',
+            'longitude' => 'required|numeric',
+            'latitude' => 'required|numeric',
+            'city' => 'required|string',
+            'country' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->jsonResponse($validator->errors(), 'data', Response::HTTP_BAD_REQUEST, 'Validation error');
+        }
+
+        $location_id = $this->saveLocation($request->latitude, $request->longitude, $request->city, $request->country);
+
+        $event = new Event();
+
+        $event->user_id = Auth::id();
+        $event->location_id = $location_id;
+        $event->name = $request->name;
+        $event->description = $request->description;
+        $event->start_date = $request->start_date;
+        $event->end_date = $request->end_date;
+
+        if ($request->has('image')) {
+            $event->image = $this->saveBase64Image($request->image, 'events' . DIRECTORY_SEPARATOR . Auth::id());
+        }
+
+        $event->save();
+
+        return $this->jsonResponse($event, 'data', Response::HTTP_CREATED, 'Event created');
     }
 
     public function show($id)
