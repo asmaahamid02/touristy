2ac93190ea5ce1811a2b59742fa4fa6107124475
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-10-30 13:14:08 +0200

    backend: modified user's relations

--- touristy-backend/app/Models/User.php
+++ touristy-backend/app/Models/User.php
@@ -113,22 +113,22 @@ class User extends Authenticatable implements JWTSubject
         return $this->belongsToMany(Comment::class, 'likes', 'user_id', 'comment_id')->withTimestamps();
     }
 
-    public function following()
+    public function followings()
     {
-        return $this->belongsToMany(User::class, 'followships', 'user_id', 'followed_user_id')->withTimestamps();
+        return $this->belongsToMany(User::class, 'followships', 'follower_user_id', 'followed_user_id')->withTimestamps();
     }
 
     public function followers()
     {
-        return $this->belongsToMany(User::class, 'followships', 'followed_user_id', 'user_id')->withTimestamps();
+        return $this->belongsToMany(User::class, 'followships', 'followed_user_id', 'follower_user_id')->withTimestamps();
     }
 
-    public function blocked_users()
+    public function blockings()
     {
         return $this->belongsToMany(User::class, 'blockships', 'user_id', 'blocked_user_id')->withTimestamps();
     }
 
-    public function blocked_by()
+    public function blockers()
     {
         return $this->belongsToMany(User::class, 'blockships', 'blocked_user_id', 'user_id')->withTimestamps();
     }
