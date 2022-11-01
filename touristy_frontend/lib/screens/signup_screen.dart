df439c9443cb7cfe83bacd41dc2f145c8aeb2b46
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-01 17:13:18 +0200

    added first signup screen with form validations

--- touristy_frontend/lib/screens/signup_screen.dart
+++ touristy_frontend/lib/screens/signup_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:touristy_frontend/widgets/logo.dart';
+import 'package:email_validator/email_validator.dart';
+
+import '../widgets/logo.dart';
+import '../screens/signup_pesronal_info.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -11,6 +14,36 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final _lastNameFocusNode = FocusNode();
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+  final _confirmPasswordFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+  bool _passwordObscureText = true;
+  bool _confirmPasswordObscureText = true;
+  String _password = '';
+  String _confirmPassword = '';
+
+  var _user = <String, Object>{};
+
+  @override
+  void dispose() {
+    _lastNameFocusNode.dispose();
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    _confirmPasswordFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _saveForm() {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState!.save();
+    Navigator.of(context).pushNamed(SignupPersonalInfo.routeName);
+  }
+
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar();
@@ -22,60 +55,159 @@ class _SignupScreenState extends State<SignupScreen> {
             const LogoHorizontal('assets/images/logo_horizontal.png', 100),
             Container(
               padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'First name',
-                      prefixIcon: Icon(Icons.person_outline),
+              child: Form(
+                key: _form,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'First name',
+                        prefixIcon: Icon(Icons.person_outline),
+                      ),
+                      autofocus: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your first name';
+                        }
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).requestFocus(_lastNameFocusNode);
+                      },
+                      onSaved: ((newValue) =>
+                          _user['first_name'] = newValue as String),
                     ),
-                    autofocus: true,
-                    onSubmitted: ((value) => {}),
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Last name',
-                      prefixIcon: Icon(Icons.person_outline),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Last name',
+                        prefixIcon: Icon(Icons.person_outline),
+                      ),
+                      focusNode: _lastNameFocusNode,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your last name';
+                        }
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).requestFocus(_emailFocusNode);
+                      },
+                      onSaved: ((newValue) =>
+                          _user['last_name'] = newValue as String),
                     ),
-                    onSubmitted: ((value) => {}),
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Email',
-                      prefixIcon: Icon(Icons.email_outlined),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email_outlined),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      focusNode: _emailFocusNode,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        const emailPattern =
+                            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+                        final result =
+                            RegExp(emailPattern, caseSensitive: false)
+                                .hasMatch(value);
+                        //check if email is valid
+                        if (!result) {
+                          return 'Please enter a valid email';
+                        }
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).requestFocus(_passwordFocusNode);
+                      },
+                      onSaved: (((newValue) =>
+                          _user['email'] = newValue as String)),
                     ),
-                    onSubmitted: ((value) => {}),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    decoration: InputDecoration(
-                      border: const UnderlineInputBorder(),
-                      labelText: 'Password',
-                      prefixIcon: const Icon(Icons.lock_outline),
-                      suffixIcon: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.remove_red_eye_outlined)),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: const UnderlineInputBorder(),
+                        labelText: 'Password',
+                        prefixIcon: const Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _passwordObscureText = !_passwordObscureText;
+                              });
+                            },
+                            icon: Icon(_passwordObscureText
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined)),
+                      ),
+                      obscureText: _passwordObscureText,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        _password = value;
+                        return null;
+                      },
+                      focusNode: _passwordFocusNode,
+                      textInputAction: TextInputAction.next,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context)
+                            .requestFocus(_confirmPasswordFocusNode);
+                      },
+                      onSaved: ((newValue) =>
+                          _user['password'] = newValue as String),
                     ),
-                    onSubmitted: ((value) => {}),
-                    keyboardType: TextInputType.visiblePassword,
-                  ),
-                ],
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: const UnderlineInputBorder(),
+                        labelText: 'Confirm Password',
+                        prefixIcon: const Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _confirmPasswordObscureText =
+                                  !_confirmPasswordObscureText;
+                            });
+                          },
+                          icon: Icon(_confirmPasswordObscureText
+                              ? Icons.visibility_off_outlined
+                              : Icons.visibility_outlined),
+                        ),
+                      ),
+                      obscureText: _confirmPasswordObscureText,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please confirm your password';
+                        }
+                        if (value != _password) {
+                          return 'Passwords do not match';
+                        }
+                        return null;
+                      },
+                      focusNode: _confirmPasswordFocusNode,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               child: ElevatedButton(
-                onPressed: () {
-                  // Navigator.of(context).pushReplacementNamed(
-                  //   HomeScreen.routeName,
-                  // );
-                },
+                onPressed: _saveForm,
                 child: const Text('NEXT'),
               ),
             ),
