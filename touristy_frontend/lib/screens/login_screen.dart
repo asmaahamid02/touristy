2366081513ac87a91540306a3c2085de02527eb7
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-01 10:41:42 +0200

    frontend: added login screen

---
+++ touristy_frontend/lib/screens/login_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../widgets/logo.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  static const routeName = '/login';
+  @override
+  Widget build(BuildContext context) {
+    final appBar = AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    );
+    return Scaffold(
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const LogoHorizontal('assets/images/logo_horizontal.png', 100),
+              Container(
+                padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+                child: Column(
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email_outlined),
+                      ),
+                      autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: const UnderlineInputBorder(),
+                        labelText: 'Password',
+                        prefixIcon: const Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.remove_red_eye_outlined),
+                          onPressed: () {},
+                        ),
+                      ),
+                      onSubmitted: ((value) => {}),
+                      keyboardType: null,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Navigator.of(context).pushReplacementNamed(
+                    //   HomeScreen.routeName,
+                    // );
+                  },
+                  child: const Text('LOGIN'),
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
+}
