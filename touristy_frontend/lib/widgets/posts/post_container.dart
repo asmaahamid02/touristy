57fda80e9314a266e49ee85b24a263113481aedb
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-04 17:36:58 +0200

    frontend: added post stats

--- touristy_frontend/lib/widgets/posts/post_container.dart
+++ touristy_frontend/lib/widgets/posts/post_container.dart
@@ -22,7 +22,7 @@ class PostContainer extends StatelessWidget {
                   _PostHeader(
                     post: post,
                   ),
-                  const SizedBox(height: 4.0),
+                  const SizedBox(height: 6.0),
                   Text(post.caption),
                   const SizedBox(height: 6.0),
                   post.imageUrl != ''
@@ -39,14 +39,19 @@ class PostContainer extends StatelessWidget {
                   child: Image.network(post.imageUrl, fit: BoxFit.cover),
                 )
               : const SizedBox.shrink(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: _PostStats(post: post),
+          )
         ],
       ),
     );
   }
 }
 
+//post header widget
 class _PostHeader extends StatelessWidget {
-  const _PostHeader({super.key, required this.post});
+  const _PostHeader({required this.post});
   final Post post;
   @override
   Widget build(BuildContext context) {
@@ -56,15 +61,32 @@ class _PostHeader extends StatelessWidget {
           imageUrl: post.user.imageUrl,
           radius: 20,
         ),
-        SizedBox(width: 8.0),
+        const SizedBox(width: 8.0),
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                post.user.name,
-                style: Theme.of(context).textTheme.headline6,
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  post.user.name,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
               ),
+              post.location != ''
+                  ? Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Row(children: [
+                        Icon(Icons.location_on,
+                            color: Colors.red[600], size: 12.0),
+                        const SizedBox(width: 5.0),
+                        Text(
+                          post.location,
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                      ]),
+                    )
+                  : const SizedBox.shrink(),
               Row(
                 children: [
                   Text(
@@ -97,6 +119,89 @@ class _PostHeader extends StatelessWidget {
           ],
         ),
       ],
+    );
+  }
+}
+
+//post stats widget
+class _PostStats extends StatelessWidget {
+  final Post post;
+
+  const _PostStats({required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('${post.likes} Likes',
+                style: Theme.of(context).textTheme.bodySmall),
+            Text('${post.comments} Comments',
+                style: Theme.of(context).textTheme.bodySmall),
+          ],
+        ),
+        const Divider(),
+        Row(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                _PostButton(
+                    icon: Icon(
+                      Icons.favorite_border_outlined,
+                      size: 20.0,
+                      color: Colors.grey[600],
+                    ),
+                    label: 'Like',
+                    onTap: () {}),
+                _PostButton(
+                    icon: Icon(Icons.message_outlined,
+                        size: 20.0, color: Colors.grey[600]),
+                    label: 'Comment',
+                    onTap: () {}),
+              ],
+            )),
+            _PostButton(
+                icon: Icon(Icons.near_me_outlined,
+                    size: 20.0, color: Colors.grey[600]),
+                label: 'Share',
+                onTap: () {}),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+//post button widget
+class _PostButton extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const _PostButton(
+      {required this.icon, required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          height: 50.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              icon,
+              const SizedBox(width: 4.0),
+              Text(label, style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
