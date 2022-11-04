43e54df53d9ce806614889ee3686d8bde87ad716
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-04 13:51:26 +0200

    frontend: changed to sliver scroll view

---
+++ touristy_frontend/lib/screens/home_pages/for_you_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../../widgets/posts/post_list.dart';
+import '../../widgets/travelers_avatars_list.dart';
+import '../../widgets/trips_list.dart';
+import '../../widgets/events_list.dart';
+
+class ForYouPage extends StatelessWidget {
+  const ForYouPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+          sliver: SliverToBoxAdapter(
+            child: TravelersAvatarsList(),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
+          sliver: SliverToBoxAdapter(
+            child: TripsList(),
+          ),
+        ),
+        SliverPadding(
+            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
+            sliver: SliverToBoxAdapter(
+              child: EventsList(),
+            )),
+      ],
+    );
+  }
+}
