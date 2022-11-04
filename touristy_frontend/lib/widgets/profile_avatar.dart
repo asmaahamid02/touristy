cd88e668428c1dab564c4e72252175606c122d8c
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-04 17:38:47 +0200

    frontend: moved avatar widget to common one

---
+++ touristy_frontend/lib/widgets/profile_avatar.dart
@@ -0,0 +1,67 @@
+import 'package:flag/flag.dart';
+import 'package:flutter/material.dart';
+
+class ProfileAvatar extends StatelessWidget {
+  const ProfileAvatar({
+    super.key,
+    this.imageUrl = '',
+    this.isActive = false,
+    this.hasFlag = false,
+    this.countryCode = '',
+    this.radius = 30.0,
+  });
+  final String imageUrl;
+  final bool isActive;
+  final bool hasFlag;
+  final String countryCode;
+  final double radius;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CircleAvatar(
+          radius: radius,
+          backgroundImage: imageUrl == ''
+              ? const AssetImage('assets/images/profile_picture.png')
+                  as ImageProvider
+              : NetworkImage(imageUrl),
+        ),
+        isActive
+            ? Positioned(
+                bottom: 0,
+                left: 0,
+                child: Container(
+                  width: 15,
+                  height: 15,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                ),
+              )
+            : Container(),
+        hasFlag
+            ? Positioned(
+                top: 0,
+                right: 0,
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Flag.fromString(
+                      countryCode,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              )
+            : Container(),
+      ],
+    );
+  }
+}
