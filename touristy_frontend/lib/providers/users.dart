296ce8c50708aa88165b6ecced0a532307ec8580
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-05 23:29:32 +0200

    frontend: added user provider

---
+++ touristy_frontend/lib/providers/users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import './auth.dart';
+import '../models/user.dart';
+import '../services/users_service.dart';
+
+class Users with ChangeNotifier {
+  List<User> _users = [];
+
+  String? authToken;
+
+  void update(Auth auth) {
+    final String newAuthToken = auth.token as String;
+    authToken = newAuthToken;
+  }
+
+  List<User> get users {
+    return [..._users];
+  }
+
+  //get users
+  Future<void> fetchAndSetUsers() async {
+    final users = await UsersService().getUsers(authToken as String);
+
+    _users = users;
+
+    notifyListeners();
+  }
+}
