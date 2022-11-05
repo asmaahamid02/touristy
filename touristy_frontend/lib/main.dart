754f629b7120064203cf359ace678900e2d2a80e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-05 23:25:52 +0200

    frontend: added auth provider and consumer for auth

--- touristy_frontend/lib/main.dart
+++ touristy_frontend/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:touristy_frontend/screens/tabs.dart';
+import 'package:provider/provider.dart';
+import './screens/tabs.dart';
 import './screens/home_screen.dart';
 import './screens/auth/signup_location_screen.dart';
 import './screens/auth/signup_profile_screen.dart';
 import './screens/auth/signup_pesronal_info_screen.dart';
-import 'screens/auth/login_screen.dart';
+import './screens/auth/login_screen.dart';
 import './screens/auth/signup_screen.dart';
 import './screens/landing_screen.dart';
+
+import './providers/auth.dart';
 
 void main() {
   //portrait mode only
@@ -22,86 +25,96 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Touristy',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.lightBlue,
-        fontFamily: 'Raleway',
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-              const EdgeInsets.symmetric(
-                vertical: 20,
-                horizontal: 40,
-              ),
-            ),
-            //secondary background color
-            backgroundColor: MaterialStateProperty.all<Color>(
-              Colors.lightBlue,
-            ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<Auth>(
+          create: (_) => Auth(),
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'Touristy',
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors.lightBlue,
+            fontFamily: 'Raleway',
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                  const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 40,
+                  ),
+                ),
+                //secondary background color
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  Colors.lightBlue,
+                ),
 
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-            shape: MaterialStateProperty.all<OutlinedBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                minimumSize: MaterialStateProperty.all<Size>(
+                  const Size(double.infinity, 36),
+                ),
               ),
             ),
-            minimumSize: MaterialStateProperty.all<Size>(
-              const Size(double.infinity, 36),
+            textTheme: const TextTheme(
+              button: TextStyle(
+                // fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              headline5: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              headline6: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ),
-        textTheme: const TextTheme(
-          button: TextStyle(
-            // fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-          headline5: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-          headline6: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        iconTheme: IconThemeData(
-          size: 24,
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all<Color>(
-              Colors.black,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              centerTitle: true,
+              elevation: 0,
+              iconTheme: IconThemeData(
+                color: Colors.black,
+              ),
+              titleTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            iconTheme: const IconThemeData(
+              size: 24,
+            ),
+            textButtonTheme: TextButtonThemeData(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(
+                  Colors.black,
+                ),
+              ),
+            ),
+            scaffoldBackgroundColor: Colors.grey[100],
           ),
+          home: auth.isAuth ? const Tabs() : const LandingScreen(),
+          routes: {
+            LoginScreen.routeName: (ctx) => const LoginScreen(),
+            SignupScreen.routeName: (ctx) => const SignupScreen(),
+            SignupPersonalInfoScreen.routeName: (ctx) =>
+                const SignupPersonalInfoScreen(),
+            SignupProfileScreen.routeName: (ctx) => const SignupProfileScreen(),
+            SignupLocationScreen.routeName: (ctx) =>
+                const SignupLocationScreen(),
+            HomeScreen.routeName: (ctx) => const HomeScreen(),
+            Tabs.routeName: (ctx) => const Tabs(),
+          },
         ),
-        scaffoldBackgroundColor: Colors.grey[100],
       ),
-      // home: LandingScreen(),
-      routes: {
-        LandingScreen.routeName: (ctx) => LandingScreen(),
-        LoginScreen.routeName: (ctx) => LoginScreen(),
-        SignupScreen.routeName: (ctx) => SignupScreen(),
-        SignupPersonalInfoScreen.routeName: (ctx) => SignupPersonalInfoScreen(),
-        SignupProfileScreen.routeName: (ctx) => SignupProfileScreen(),
-        SignupLocationScreen.routeName: (ctx) => SignupLocationScreen(),
-        HomeScreen.routeName: (ctx) => HomeScreen(),
-        Tabs.routeName: (ctx) => Tabs(),
-      },
     );
   }
 }
