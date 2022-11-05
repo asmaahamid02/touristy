a013b4d3dea6d4de8ce8411bcd1852274f34030b
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-05 23:24:13 +0200

    frontend: linked login to backend

--- touristy_frontend/lib/screens/auth/login_screen.dart
+++ touristy_frontend/lib/screens/auth/login_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:touristy_frontend/screens/tabs.dart';
+import 'package:provider/provider.dart';
 import '../../widgets/common_buttons.dart';
 import '../../widgets/logo.dart';
+
+import '../../providers/auth.dart';
+
+import '../../models/http_exception.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -22,7 +26,7 @@ class _LoginScreenState extends State<LoginScreen> {
   bool _passwordObscureText = true;
 
   final _user = <String, Object>{};
-
+  var _isLoading = false;
   @override
   void dispose() {
     _emailFocusNode.dispose();
@@ -30,16 +34,61 @@ class _LoginScreenState extends State<LoginScreen> {
     super.dispose();
   }
 
-  void _saveForm() {
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('An error occurred!',
+            style: Theme.of(context).textTheme.headline5),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+            style: Theme.of(context).textButtonTheme.style?.copyWith(
+                  backgroundColor: MaterialStateProperty.all(
+                    Theme.of(context).errorColor,
+                  ),
+                  foregroundColor: MaterialStateProperty.all(
+                    Colors.white,
+                  ),
+                ),
+            child: const Text('Okay'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _saveForm() async {
     final isValid = _form.currentState!.validate();
     if (!isValid) {
       return;
     }
     _form.currentState!.save();
-    Navigator.of(context)
-        .pushReplacementNamed(Tabs.routeName, arguments: _user);
 
-    print(_user);
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      await Provider.of<Auth>(context, listen: false)
+          .login(_user['email'] as String, _user['password'] as String);
+
+      if (!mounted) return;
+      Navigator.of(context).pushReplacementNamed('/');
+    } on HttpException catch (error) {
+      final errorMessage = error.toString();
+      _showErrorDialog(errorMessage);
+    } catch (error) {
+      const errorMessage = 'Authentication failed';
+      _showErrorDialog(errorMessage);
+    }
+    setState(() {
+      _isLoading = false;
+    });
+    // Navigator.of(context)
+    //     .pushReplacementNamed(Tabs.routeName, arguments: _user);
   }
 
   @override
@@ -66,10 +115,13 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              PrimaryButton(
-                textLabel: 'Login',
-                onTap: _saveForm,
-              ),
+              const SizedBox(height: 20),
+              _isLoading
+                  ? const CircularProgressIndicator()
+                  : PrimaryButton(
+                      textLabel: 'Login',
+                      onTap: _saveForm,
+                    ),
             ]),
       ),
     );
