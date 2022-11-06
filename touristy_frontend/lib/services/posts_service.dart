a1debfc88587fb074df228fc551cd70c5c254887
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-06 21:08:13 +0200

    frontend: added toggleLikePost method

--- touristy_frontend/lib/services/posts_service.dart
+++ touristy_frontend/lib/services/posts_service.dart
@@ -31,4 +31,32 @@ class PostsService {
       rethrow;
     }
   }
+
+  //toggle like post
+  Future<bool> toggleLikePost(String token, int postId) async {
+    try {
+      final response =
+          await http.get(Uri.parse('$baseUrl/posts/like/$postId'), headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+        HttpHeaders.acceptHeader: 'application/json',
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      });
+
+      final responseData = json.decode(response.body) as Map<String, dynamic>;
+
+      //check if response is has error, throw exception
+      if (response.statusCode != 200) {
+        throw HttpException(getResponseError(responseData));
+      } else {
+        String message = responseData['message'];
+        //check if message contains liked unliked
+        if (message.contains('liked')) {
+          return true;
+        }
+        return false;
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
 }
