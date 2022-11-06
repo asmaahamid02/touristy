dc07d9d4218fab6bfc37f99f098a962f420483d7
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-06 15:30:14 +0200

    frontend: added file url const

--- touristy_frontend/lib/constants.dart
+++ touristy_frontend/lib/constants.dart
@@ -3,6 +3,7 @@
 import 'dart:math';
 
 const String baseUrl = 'http://10.0.2.2:8000/api/v0.1';
+const String fileBaseUrl = 'http://10.0.2.2:8000/api/v0.1/common/file/';
 const String loginUrl = '$baseUrl/auth/login';
 const String registerUrl = '$baseUrl/auth/register';
 
@@ -13,7 +14,7 @@ const String unauthorizedError = 'Unauthorized';
 const String somethingWentWrongError = 'Something went wrong';
 
 //----METHODS-----
-//randomly select number of items from a list
+//randomly select number of distinct items from a list
 List<T> randomItems<T>(List<T> items, int count) {
   var random = Random();
   var result = <T>[];
@@ -21,4 +22,16 @@ List<T> randomItems<T>(List<T> items, int count) {
     result.add(items[random.nextInt(items.length)]);
   }
   return result;
+}
+
+//get response error
+String getResponseError(dynamic responseData) {
+  String error = '';
+  if (responseData['error'] != null) {
+    final errors = responseData['error'];
+    error = errors[errors.keys.first][0];
+  } else {
+    error = responseData['message'];
+  }
+  return error;
 }
