49a45dc27b1a0be7e668d06e485ef256b8b16c18
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-06 15:38:13 +0200

    frontend: displayed posts from provider

--- touristy_frontend/lib/widgets/posts/post_list.dart
+++ touristy_frontend/lib/widgets/posts/post_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:touristy_frontend/models/user.dart';
-import '../../models/post.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/http_exception.dart';
+import '../../providers/posts.dart';
 import './post_container.dart';
 
 class PostsList extends StatefulWidget {
@@ -11,90 +13,56 @@ class PostsList extends StatefulWidget {
 }
 
 class _PostsListState extends State<PostsList> {
-  List<Post> _posts = [
-    Post(
-      id: '1',
-      imageUrl: 'https://picsum.photos/201',
-      caption: 'This is a caption',
-      location: 'This is a location',
-      user: User(
-        id: '1',
-        name: 'Michael',
-        imageUrl: 'https://picsum.photos/202',
+  var isInit = true;
+  var _isLoading = false;
+
+  void _showSnakeBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).errorColor,
       ),
-      likes: 100,
-      comments: 20,
-      shares: 10,
-      timeAgo: '1day ago',
-    ),
-    Post(
-      id: '2',
-      imageUrl: 'https://picsum.photos/203',
-      caption: 'This is a caption',
-      location: 'This is a location',
-      user: User(
-        id: '2',
-        name: 'John Doe',
-        imageUrl: 'https://picsum.photos/204',
-      ),
-      likes: 100,
-      comments: 20,
-      shares: 10,
-      timeAgo: '1hr ago',
-    ),
-    Post(
-      id: '3',
-      imageUrl: 'https://picsum.photos/205',
-      caption: 'This is a caption',
-      location: 'This is a location',
-      user: User(
-        id: '3',
-        name: 'Klaus',
-        imageUrl: 'https://picsum.photos/206',
-      ),
-      likes: 100,
-      comments: 20,
-      shares: 10,
-      timeAgo: '53min ago',
-    ),
-    Post(
-      id: '4',
-      imageUrl: 'https://picsum.photos/250?image=9',
-      caption: 'This is a caption',
-      location: 'This is a location',
-      user: User(
-        id: '4',
-        name: 'Alex Doe',
-        imageUrl: 'https://picsum.photos/208',
-      ),
-      likes: 100,
-      comments: 20,
-      shares: 10,
-      timeAgo: '2hr ago',
-    ),
-    Post(
-      id: '5',
-      imageUrl: 'https://picsum.photos/209',
-      caption: 'This is a caption',
-      location: 'This is a location',
-      user: User(
-        id: '5',
-        name: 'Frank',
-        imageUrl: 'https://picsum.photos/210',
-      ),
-      likes: 100,
-      comments: 20,
-      shares: 10,
-      timeAgo: '1day ago',
-    ),
-  ];
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      try {
+        Provider.of<Posts>(context).fetchAndSetPosts().then((_) {
+          setState(() {
+            _isLoading = false;
+          });
+        });
+      } on HttpException catch (error) {
+        _showSnakeBar(error.toString());
+      } catch (error) {
+        _showSnakeBar('Could not fetch posts. Please try again later.');
+      }
+      isInit = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-        delegate: SliverChildBuilderDelegate((context, index) {
-      return PostContainer(
-        post: _posts[index],
-      );
-    }, childCount: _posts.length));
+    final postsData = Provider.of<Posts>(context);
+    final posts = postsData.posts;
+    return _isLoading
+        ? const SliverToBoxAdapter(
+            child: Center(child: CircularProgressIndicator()))
+        : SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return PostContainer(
+                  post: posts[index],
+                );
+              },
+              childCount: posts.length,
+            ),
+          );
   }
 }
