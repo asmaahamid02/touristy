4424f53ddf5a0d2c499a8f2f84380b9111bd48ff
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-06 15:27:31 +0200

    frontend: added post service/get posts function

---
+++ touristy_frontend/lib/services/posts_service.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../constants.dart';
+
+import '../models/post.dart';
+
+class PostsService {
+  Future<List<Post>> getPosts(String token) async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/posts'), headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+        HttpHeaders.acceptHeader: 'application/json',
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      });
+      final responseData = json.decode(response.body) as Map<String, dynamic>;
+
+      //check if response is has error, throw exception
+      if (response.statusCode != 200) {
+        throw HttpException(getResponseError(responseData));
+      } else {
+        if (responseData['data'] != null) {
+          final List<dynamic> postsList = responseData['data'];
+          return postsList.map((post) => Post.fromJson(post)).toList();
+        } else {
+          return [];
+        }
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
