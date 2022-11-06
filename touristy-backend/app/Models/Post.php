fbf037770e7c875e23851de399801979d1002aee
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-06 21:11:02 +0200

    backend: added with time stamps

--- touristy-backend/app/Models/Post.php
+++ touristy-backend/app/Models/Post.php
@@ -45,7 +45,7 @@ class Post extends Model
 
     public function likes()
     {
-        return $this->belongsToMany(User::class, 'likes', 'post_id', 'user_id');
+        return $this->belongsToMany(User::class, 'likes', 'post_id', 'user_id')->withTimestamps();
     }
 
     public function media()
