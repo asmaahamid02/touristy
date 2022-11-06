8c9dc0e57ca8ca255b1a5fd99094a8846ed7c088
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-07 00:07:03 +0200

    frontend: added follow user request

--- touristy_frontend/lib/services/users_service.dart
+++ touristy_frontend/lib/services/users_service.dart
@@ -17,17 +17,21 @@ class UsersService {
           responseData['data'].length > 0) {
         final usersList = responseData['data'];
 
+//get following users
         response = await http.get(Uri.parse('$baseUrl/users/followings'),
             headers: getHeaders(token));
 
         final followingData =
             json.decode(response.body) as Map<String, dynamic>;
+
+        //map data to get user ids
         final followingsList = followingData['data'] != null &&
                 followingData['data'] != '' &&
                 followingData['data'].length > 0
             ? followingData['data'].map((e) => e['id']).toList()
             : [];
 
+//map user list to get user model
         return usersList.map<User>((user) {
           user['isFollowing'] = followingsList.length <= 0
               ? false
@@ -39,6 +43,18 @@ class UsersService {
       }
     } else {
       throw Exception('Failed to load users');
+    }
+  }
+
+  //follow user
+  Future<bool> followUser(String token, int userId) async {
+    var response = await http.post(Uri.parse('$baseUrl/users/follow/$userId'),
+        headers: getHeaders(token));
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
