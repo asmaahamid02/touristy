daf1b69534381674dce844c7ee004b1e68bd834f
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-07 00:06:26 +0200

    frontend: added follow user method

--- touristy_frontend/lib/providers/users.dart
+++ touristy_frontend/lib/providers/users.dart
@@ -7,10 +7,12 @@ class Users with ChangeNotifier {
   List<User> _users = [];
 
   String? authToken;
+  int? currentUserId;
 
   void update(Auth auth) {
     if (auth.token != null) {
       authToken = auth.token;
+      currentUserId = auth.userId;
       notifyListeners();
     }
   }
@@ -30,5 +32,22 @@ class Users with ChangeNotifier {
 
   bool isFollowed(int userId) {
     return _users.any((user) => user.id == userId && user.isFollowing == true);
+  }
+
+  //follow user
+  Future<void> followUser(int userId) async {
+    final int userIndex = _users.indexWhere((user) => user.id == userId);
+
+    if (userIndex >= 0) {
+      _users[userIndex].isFollowing = !_users[userIndex].isFollowing!;
+      notifyListeners();
+
+      try {
+        await UsersService().followUser(authToken as String, userId);
+      } catch (error) {
+        _users[userIndex].isFollowing = !_users[userIndex].isFollowing!;
+        notifyListeners();
+      }
+    }
   }
 }
