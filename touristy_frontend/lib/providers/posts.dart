9f59727cd9a69dc835b6aea99b8f364fffce6265
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-06 21:07:29 +0200

    frontend: added toggleLikeStatus method

--- touristy_frontend/lib/providers/posts.dart
+++ touristy_frontend/lib/providers/posts.dart
@@ -7,10 +7,14 @@ class Posts with ChangeNotifier {
   List<Post> _posts = [];
 
   String? authToken;
+  int? userId;
 
-  void update(Auth auth) {
+  void update(
+    Auth auth,
+  ) {
     if (auth.token != null) {
       authToken = auth.token;
+      userId = auth.userId;
       notifyListeners();
     }
   }
@@ -26,5 +30,27 @@ class Posts with ChangeNotifier {
     _posts = posts;
 
     notifyListeners();
+  }
+
+  Future<void> toggleLikeStatus(int postId) async {
+    final int postIndex = _posts.indexWhere((post) => post.id == postId);
+
+    if (postIndex >= 0) {
+      _posts[postIndex].isLiked = !_posts[postIndex].isLiked!;
+      _posts[postIndex].likes = _posts[postIndex].isLiked!
+          ? _posts[postIndex].likes! + 1
+          : _posts[postIndex].likes! - 1;
+      notifyListeners();
+
+      try {
+        await PostsService().toggleLikePost(authToken as String, postId);
+      } catch (error) {
+        _posts[postIndex].isLiked = !_posts[postIndex].isLiked!;
+        _posts[postIndex].likes = _posts[postIndex].isLiked!
+            ? _posts[postIndex].likes! + 1
+            : _posts[postIndex].likes! - 1;
+        notifyListeners();
+      }
+    }
   }
 }
