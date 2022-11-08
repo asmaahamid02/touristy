540c2bc61b9aa46b20bd1d0ccf4fc7431b6c85d0
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-08 13:41:10 +0200

    frontend: added future buider

--- touristy_frontend/lib/widgets/travelers_avatars_list.dart
+++ touristy_frontend/lib/widgets/travelers_avatars_list.dart
@@ -3,30 +3,13 @@ import 'package:provider/provider.dart';
 import '../providers/users.dart';
 import './profile_avatar.dart';
 
-class TravelersAvatarsList extends StatefulWidget {
+class TravelersAvatarsList extends StatelessWidget {
   const TravelersAvatarsList({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<TravelersAvatarsList> createState() => _TravelersAvatarsListState();
-}
-
-class _TravelersAvatarsListState extends State<TravelersAvatarsList> {
-  var _isInit = true;
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (_isInit) {
-      Provider.of<Users>(context).fetchAndSetUsers();
-    }
-    _isInit = false;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final usersData = Provider.of<Users>(context);
-    final users = usersData.users;
     return Container(
       height: 140,
       padding: const EdgeInsets.only(bottom: 10),
@@ -41,24 +24,42 @@ class _TravelersAvatarsListState extends State<TravelersAvatarsList> {
               style: Theme.of(context).textTheme.headline5,
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: ProfileAvatar(
-                    countryCode: users[index].countryCode,
-                    imageUrl: users[index].profilePictureUrl as String,
-                    isActive: true,
-                    hasFlag: true,
-                  ),
-                );
-              },
-              itemCount: users.length,
-              padding: const EdgeInsets.symmetric(horizontal: 4.0),
-              scrollDirection: Axis.horizontal,
-            ),
-          ),
+          FutureBuilder(
+            future:
+                Provider.of<Users>(context, listen: false).fetchAndSetUsers(),
+            builder: ((context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                if (snapshot.error != null) {
+                  return const Center(child: Text('An error occured, Refresh'));
+                } else {
+                  return Consumer<Users>(
+                    builder: (context, usersData, child) => Expanded(
+                      child: ListView.builder(
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: ProfileAvatar(
+                              countryCode: usersData.users[index].countryCode,
+                              imageUrl: usersData.users[index].profilePictureUrl
+                                  as String,
+                              isActive: true,
+                              hasFlag: true,
+                            ),
+                          );
+                        },
+                        itemCount: usersData.users.length,
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        scrollDirection: Axis.horizontal,
+                      ),
+                    ),
+                  );
+                }
+              }
+            }),
+          )
         ],
       ),
     );
