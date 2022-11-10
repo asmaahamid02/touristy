6919b083ff1ba0ceef6450b8693e464fa2d44e3b
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-10 23:10:51 +0200

    frontend: styled the primary button

--- touristy_frontend/lib/widgets/common_buttons.dart
+++ touristy_frontend/lib/widgets/common_buttons.dart
@@ -14,9 +14,19 @@ class PrimaryButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+      ),
       child: ElevatedButton(
         onPressed: onTap,
-        child: Text(textLabel),
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+          minimumSize: const Size(double.infinity, 50),
+        ),
+        child: Text(textLabel.toUpperCase(),
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
       ),
     );
   }
