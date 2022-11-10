8508c15411ce64bd73b479736cfceb9f6eac1bbf
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-10 22:27:51 +0200

    backend: added post seeder

---
+++ touristy-backend/database/seeders/PostSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\PostMedia;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::factory(100)->create()->each(
+            function ($post) {
+                $post->media()->saveMany(
+                    PostMedia::factory(rand(1, 5))->create([
+                        'post_id' => $post->id,
+                    ])
+                );
+            }
+        );
+    }
+}
