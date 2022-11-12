5f4fbc0293b910579d6ebb0dbe3d35b2ed8b1669
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-12 18:41:33 +0200

    frontend: added story data model

---
+++ touristy_frontend/lib/models/story_data.dart
@@ -0,0 +1,13 @@
+class StoryData {
+  String? name;
+  String? url;
+  String? countryCode;
+  bool? isOnline;
+
+  StoryData({
+    this.name,
+    this.url,
+    this.countryCode,
+    this.isOnline,
+  });
+}
