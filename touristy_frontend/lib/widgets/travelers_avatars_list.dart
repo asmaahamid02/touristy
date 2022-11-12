33431a04edd0b196760fca21d43976c81a003873
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-12 18:43:07 +0200

    frontend: fetched data from travelers users provider

--- touristy_frontend/lib/widgets/travelers_avatars_list.dart
+++ touristy_frontend/lib/widgets/travelers_avatars_list.dart
@@ -1,65 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/users.dart';
-import './profile_avatar.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import '../exceptions/http_exception.dart';
 
-class TravelersAvatarsList extends StatelessWidget {
+import '../providers/providers.dart';
+import '../models/models.dart';
+import './widgets.dart';
+
+class TravelersAvatarsList extends StatefulWidget {
   const TravelersAvatarsList({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TravelersAvatarsList> createState() => _TravelersAvatarsListState();
+}
+
+class _TravelersAvatarsListState extends State<TravelersAvatarsList> {
+  var isInit = true;
+  var _isLoading = false;
+
+  void _showToastMessage(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+    if (isInit) {
+      _isLoading = true;
+
+      try {
+        await Provider.of<TravelersUsers>(context, listen: false)
+            .fetchTravelersUsers();
+      } on HttpException catch (error) {
+        _showToastMessage(error.toString());
+      } catch (error) {
+        _showToastMessage('Could not fetch Users, try again.');
+      }
+      if (!mounted) return;
+      setState(() {
+        _isLoading = false;
+      });
+      isInit = false;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 140,
-      padding: const EdgeInsets.only(bottom: 10),
-      color: Colors.white,
+    final travelersUsersData = Provider.of<TravelersUsers>(context);
+    final travelersUsers = travelersUsersData.travelersUsers;
+    return Card(
+      elevation: 0,
+      margin: const EdgeInsets.all(0),
       child: Column(
         children: [
           Container(
             alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Text(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: const Text(
               'Travelers around the world',
-              style: Theme.of(context).textTheme.headline5,
+              style: TextStyle(fontSize: 24.0),
             ),
           ),
-          FutureBuilder(
-            future:
-                Provider.of<Users>(context, listen: false).fetchAndSetUsers(),
-            builder: ((context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                if (snapshot.error != null) {
-                  return const Center(child: Text('An error occured, Refresh'));
-                } else {
-                  return Consumer<Users>(
-                    builder: (context, usersData, child) => Expanded(
-                      child: ListView.builder(
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: ProfileAvatar(
-                              countryCode: usersData.users[index].countryCode,
-                              imageUrl: usersData.users[index].profilePictureUrl
-                                  as String,
-                              isActive: true,
-                              hasFlag: true,
-                            ),
-                          );
-                        },
-                        itemCount: usersData.users.length,
-                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                        scrollDirection: Axis.horizontal,
-                      ),
-                    ),
-                  );
-                }
-              }
-            }),
-          )
+          _isLoading
+              ? const SizedBox(
+                  height: 100,
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                )
+              : Stories(
+                  storiesData: travelersUsers
+                      .map((user) => StoryData(
+                          name: user.fullName,
+                          url: user.profilePictureUrl,
+                          countryCode: user.countryCode))
+                      .toList(),
+                ),
         ],
       ),
     );
