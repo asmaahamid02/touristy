645d8fd993b569e991b5ee5ad42febb7a6ea9559
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-12 18:42:19 +0200

    frontend: added travelers users service

---
+++ touristy_frontend/lib/services/travelers_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:touristy_frontend/exceptions/http_exception.dart';
+
+import '../models/models.dart';
+import '../utilities/utilities.dart';
+
+class TravelersService {
+  Future<List<TravelerUser>> getTravelersUsers(String token) async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/users/suggested/20'),
+          headers: getHeaders(token));
+      final responseData = jsonDecode(response.body) as Map<String, dynamic>;
+      if (response.statusCode != 200) {
+        throw HttpException(getResponseError(responseData));
+      } else {
+        if (responseData['data'] != null &&
+            responseData['data'] != '' &&
+            responseData['data'].length > 0) {
+          final List<dynamic> travelersList = responseData['data'];
+          final users = travelersList
+              .map((traveler) => TravelerUser.fromJson(traveler))
+              .toList();
+          return users;
+        } else {
+          return [];
+        }
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
