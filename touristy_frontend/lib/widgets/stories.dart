d0b0685f3b5907d6e470f11531ea0915f2c8e03f
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-12 18:37:56 +0200

    frontend: added stories list widget

---
+++ touristy_frontend/lib/widgets/stories.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import './widgets.dart';
+import '../models/models.dart';
+
+class Stories extends StatelessWidget {
+  const Stories({
+    Key? key,
+    required this.storiesData,
+  }) : super(key: key);
+
+  final List<StoryData> storiesData;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      margin: const EdgeInsets.all(0),
+      child: Container(
+        height: 110,
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemCount: storiesData.length,
+          itemBuilder: (context, index) {
+            return StoryCard(storyData: storiesData[index]);
+          },
+        ),
+      ),
+    );
+  }
+}
