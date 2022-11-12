0479654d3a9fba4e70faa60f49e5c80f54e34d30
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-12 18:41:19 +0200

    frontend: added message data model

---
+++ touristy_frontend/lib/models/message_data.dart
@@ -0,0 +1,15 @@
+class MessageData {
+  final String message;
+  final String senderName;
+  final DateTime messageDate;
+  final String dateMessage;
+  final String profilePicture;
+
+  MessageData({
+    required this.message,
+    required this.senderName,
+    required this.messageDate,
+    required this.dateMessage,
+    required this.profilePicture,
+  });
+}
