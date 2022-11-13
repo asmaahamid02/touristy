6910cefcd2abacc4be70d6c6a07356d2c953cc3f
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-14 00:22:18 +0200

    frontend: fetched all users posts

--- touristy_frontend/lib/screens/home_pages/for_you_page.dart
+++ touristy_frontend/lib/screens/home_pages/for_you_page.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../widgets/widgets.dart';
-
 import '../../providers/providers.dart';
+import '../../exceptions/http_exception.dart';
+import '../../utilities/utilities.dart';
 
 class ForYouPage extends StatelessWidget {
   const ForYouPage({
@@ -30,20 +31,81 @@ class ForYouPage extends StatelessWidget {
               child: TravelersAvatarsList(),
             ),
           ),
-          SliverPadding(
-            padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
-            sliver: SliverToBoxAdapter(
-              child: TripsList(),
-            ),
-          ),
-          SliverPadding(
-              padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
-              sliver: SliverToBoxAdapter(
-                child: EventsList(),
-              )),
-          PostsList(),
+          // SliverPadding(
+          //   padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
+          //   sliver: SliverToBoxAdapter(
+          //     child: TripsList(),
+          //   ),
+          // ),
+          // SliverPadding(
+          //     padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 5.0),
+          //     sliver: SliverToBoxAdapter(
+          //       child: EventsList(),
+          //     )),
+          _PostsList(),
         ],
       ),
     );
+  }
+}
+
+class _PostsList extends StatefulWidget {
+  const _PostsList();
+
+  @override
+  State<_PostsList> createState() => __PostsListState();
+}
+
+class __PostsListState extends State<_PostsList> {
+  var isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+    if (isInit) {
+      _isLoading = true;
+
+      try {
+        await Provider.of<Posts>(context).fetchAndSetPosts();
+      } on HttpException catch (error) {
+        SnakeBar.show(context, error.toString());
+      } catch (error) {
+        SnakeBar.show(context, 'Could not fetch posts, try again later.');
+      }
+      setState(() {
+        _isLoading = false;
+      });
+
+      isInit = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final postsData = Provider.of<Posts>(context);
+    final posts = postsData.posts;
+
+    if (posts.isEmpty) {
+      return const SliverToBoxAdapter(
+        child: Center(
+          child: Text('No posts yet.'),
+        ),
+      );
+    } else {
+      return _isLoading
+          ? const SliverToBoxAdapter(
+              child: Center(child: CircularProgressIndicator()),
+            )
+          : SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  final post = posts[index];
+                  return PostContainer(post: post);
+                },
+                childCount: posts.length,
+              ),
+            );
+    }
   }
 }
