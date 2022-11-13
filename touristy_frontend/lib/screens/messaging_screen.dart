d79b6f2392818bc6f207ceb57c4118e5c6ad2d4e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-13 19:18:33 +0200

    frontend: displayed list of users who messaged the current user from firebase

--- touristy_frontend/lib/screens/messaging_screen.dart
+++ touristy_frontend/lib/screens/messaging_screen.dart
@@ -1,85 +1,166 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import '../../models/models.dart';
 import '../widgets/widgets.dart';
+import '../providers/providers.dart';
 
-class MessagingScreen extends StatelessWidget {
-  MessagingScreen({super.key});
+class MessagingScreen extends StatefulWidget {
+  const MessagingScreen({super.key});
   static const String routeName = '/messaging';
+
+  @override
+  State<MessagingScreen> createState() => _MessagingScreenState();
+}
+
+class _MessagingScreenState extends State<MessagingScreen> {
   final TextEditingController _searchController = TextEditingController();
+  int? currentUserId;
+  List<int> friendsIds = [];
+  bool _isInit = true;
+  bool _isLoading = false;
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+
+    //fetch users from provider
+    if (_isInit) {
+      try {
+        currentUserId = Provider.of<Auth>(context, listen: false).userId;
+
+        setState(() {
+          _isLoading = true;
+        });
+        await Provider.of<Users>(context, listen: false).fetchAndSetUsers();
+      } catch (error) {
+        //show error snackbar
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(error.toString()),
+          ),
+        );
+      }
+
+      setState(() {
+        _isLoading = false;
+      });
+    }
+    _isInit = false;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final users = Provider.of<Users>(context).users;
     return Scaffold(
+      backgroundColor: Theme.of(context).cardColor,
       appBar: AppBar(
         title: const Text('Chats'),
       ),
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: CustomScrollView(
-          slivers: [
-            //search bar
-            SliverToBoxAdapter(
-              child: SearchBar(
-                searchController: _searchController,
-              ),
-            ),
-            //stories
-            SliverToBoxAdapter(
-              child: Stories(
-                storiesData: [
-                  StoryData(
-                    name: faker.person.name(),
-                    url:
-                        'https://picsum.photos/200?random=${faker.randomGenerator.integer(70)}',
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('chats')
+                  .where('chatUsers', arrayContains: currentUserId)
+                  .orderBy('latestMessage.sentAt', descending: true)
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return const Text('Something went wrong');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return GestureDetector(
+                  onTap: () => FocusScope.of(context).unfocus(),
+                  child: CustomScrollView(
+                    slivers: [
+                      //search bar
+                      SliverToBoxAdapter(
+                        child: SearchBar(
+                          searchController: _searchController,
+                        ),
+                      ),
+                      //stories
+                      SliverToBoxAdapter(
+                        child: Stories(
+                          storiesData: [
+                            StoryData(
+                              name: faker.person.name(),
+                              url:
+                                  'https://picsum.photos/200?random=${faker.randomGenerator.integer(70)}',
+                            ),
+                            StoryData(
+                              name: faker.person.name(),
+                              url:
+                                  'https://picsum.photos/200?random=${faker.randomGenerator.integer(70)}',
+                            ),
+                            StoryData(
+                              name: faker.person.name(),
+                              url:
+                                  'https://picsum.photos/200?random=${faker.randomGenerator.integer(70)}',
+                            ),
+                            StoryData(
+                              name: faker.person.name(),
+                              url:
+                                  'https://picsum.photos/200?random=${faker.randomGenerator.integer(70)}',
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      SliverPadding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 4.0),
+                        sliver: SliverList(
+                          delegate:
+                              SliverChildBuilderDelegate((context, index) {
+                            final data = snapshot.data!.docs[index].data()
+                                as Map<String, dynamic>;
+                            final User user = users.firstWhere((user) =>
+                                user.id ==
+                                data['chatUsers'].firstWhere(
+                                    (element) => element != currentUserId));
+                            return _buildMessageTile(
+                              MessageData(
+                                isRead: data['latestMessage']['isRead'],
+                                senderId: user.id,
+                                lastMessageSenderId: data['latestMessage']
+                                    ['sentBy'],
+                                profilePicture: user.profilePictureUrl ?? '',
+                                message: data['latestMessage']['text'],
+                                messageDate:
+                                    data['latestMessage']['sentAt'].toDate(),
+                                dateMessage: timeago.format(
+                                    data['latestMessage']['sentAt'].toDate()),
+                                senderName:
+                                    '${user.firstName} ${user.lastName}',
+                              ),
+                            );
+
+                            // }).toList();
+                          }, childCount: snapshot.data!.docs.length),
+                        ),
+                      )
+                    ],
                   ),
-                  StoryData(
-                    name: faker.person.name(),
-                    url:
-                        'https://picsum.photos/200?random=${faker.randomGenerator.integer(70)}',
-                  ),
-                  StoryData(
-                    name: faker.person.name(),
-                    url:
-                        'https://picsum.photos/200?random=${faker.randomGenerator.integer(70)}',
-                  ),
-                  StoryData(
-                    name: faker.person.name(),
-                    url:
-                        'https://picsum.photos/200?random=${faker.randomGenerator.integer(70)}',
-                  ),
-                ],
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(_delegate, childCount: 10
-                  // snapshot.data!.docs.length,
-                  ),
-            ),
-          ],
-        ),
-      ),
+                );
+              }),
     );
   }
 
-  Widget _delegate(BuildContext context, int index) {
-    final faker = Faker();
-    //fake datetime object between 1 and 2 years ago
-    final messageDate = faker.date
-        .dateTime(minYear: DateTime.now().year, maxYear: DateTime.now().year);
+  Widget _buildMessageTile(MessageData messageData) {
     return Card(
       elevation: 0,
       margin: const EdgeInsets.only(top: 1.0),
-      child: MessageTile(
-          messageData: MessageData(
-        message: faker.lorem.sentence(),
-        senderName: faker.person.name(),
-        messageDate: messageDate,
-        dateMessage: timeago.format(messageDate),
-        //get random image from picsum
-        profilePicture: 'https://picsum.photos/200/300?random=$index',
-      )),
+      child: MessageTile(messageData: messageData),
     );
   }
 }
