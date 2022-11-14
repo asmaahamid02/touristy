6ab3d1bdb0eeabad77ae37d170d324f2e12ccfb8
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-14 14:43:55 +0200

    frontend: fixed imports

--- touristy_frontend/lib/screens/home_screen.dart
+++ touristy_frontend/lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/auth.dart';
 import './screens.dart';
+import '../providers/providers.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
