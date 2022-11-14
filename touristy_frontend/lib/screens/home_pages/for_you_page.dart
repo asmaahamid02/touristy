f08587fba56253fdbe3cf01ebab71596aacead86
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-14 14:36:49 +0200

    frontend: added lazy load functionality to the for you page

--- touristy_frontend/lib/screens/home_pages/for_you_page.dart
+++ touristy_frontend/lib/screens/home_pages/for_you_page.dart
@@ -6,26 +6,95 @@ import '../../providers/providers.dart';
 import '../../exceptions/http_exception.dart';
 import '../../utilities/utilities.dart';
 
-class ForYouPage extends StatelessWidget {
+class ForYouPage extends StatefulWidget {
   const ForYouPage({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<ForYouPage> createState() => _ForYouPageState();
+}
+
+class _ForYouPageState extends State<ForYouPage> {
+  late ScrollController _scrollController;
+  final int maxLength = 10;
+
+  bool _isLoading = false;
+  bool _isInit = true;
+  bool _hasMore = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+              _scrollController.position.maxScrollExtent * 0.95 &&
+          !_isLoading) {
+        if (_hasMore) {
+          _loadMore();
+        }
+      }
+    });
+  }
+
+  @override
+  didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_isInit) {
+      _loadMore();
+      _isInit = false;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  void _loadMore() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final posts =
+          await Provider.of<Posts>(context, listen: false).fetchAndSetPosts();
+      setState(() {
+        _isLoading = false;
+        if (posts.length < maxLength) {
+          _hasMore = false;
+        }
+      });
+    } on HttpException catch (error) {
+      setState(() {
+        _isLoading = false;
+        _hasMore = false;
+      });
+      SnakeBarCommon.show(context, error.toString());
+    } catch (error) {
+      setState(() {
+        _isLoading = false;
+        _hasMore = false;
+      });
+      SnakeBarCommon.show(context, error.toString());
+    }
+  }
+
   Future<void> _refreshData(BuildContext context) async {
     await Provider.of<Posts>(context, listen: false).fetchAndSetPosts();
     await Provider.of<Users>(context, listen: false).fetchAndSetUsers();
-    // await Provider.of<Events>(context, listen: false).fetchAndSetEvents();
-    // await Provider.of<Trips>(context, listen: false).fetchAndSetTrips();
-    // await Provider.of<Travelers>(context, listen: false).fetchAndSetTravelers();
   }
 
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
       onRefresh: () => _refreshData(context),
-      child: const CustomScrollView(
+      child: CustomScrollView(
+        controller: _scrollController,
         slivers: [
-          SliverPadding(
+          const SliverPadding(
             padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
             sliver: SliverToBoxAdapter(
               child: TravelersAvatarsList(),
@@ -42,70 +111,41 @@ class ForYouPage extends StatelessWidget {
           //     sliver: SliverToBoxAdapter(
           //       child: EventsList(),
           //     )),
-          _PostsList(),
+          _PostsList(_hasMore),
         ],
       ),
     );
   }
 }
 
-class _PostsList extends StatefulWidget {
-  const _PostsList();
-
-  @override
-  State<_PostsList> createState() => __PostsListState();
-}
-
-class __PostsListState extends State<_PostsList> {
-  var isInit = true;
-  var _isLoading = false;
-
-  @override
-  void didChangeDependencies() async {
-    super.didChangeDependencies();
-    if (isInit) {
-      _isLoading = true;
-
-      try {
-        await Provider.of<Posts>(context).fetchAndSetPosts();
-      } on HttpException catch (error) {
-        SnakeBar.show(context, error.toString());
-      } catch (error) {
-        SnakeBar.show(context, 'Could not fetch posts, try again later.');
-      }
-      setState(() {
-        _isLoading = false;
-      });
-
-      isInit = false;
-    }
-  }
+class _PostsList extends StatelessWidget {
+  const _PostsList(this._hasMore);
+  final _hasMore;
 
   @override
   Widget build(BuildContext context) {
-    final postsData = Provider.of<Posts>(context);
-    final posts = postsData.posts;
-
-    if (posts.isEmpty) {
-      return const SliverToBoxAdapter(
-        child: Center(
-          child: Text('No posts yet.'),
+    return Consumer<Posts>(
+      builder: (context, postsData, _) => SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            if (index == postsData.posts.length) {
+              return SizedBox(
+                height: 50,
+                child: Center(
+                  child: _hasMore
+                      ? const CircularProgressIndicator()
+                      : const Center(
+                          child: Text('No more posts'),
+                        ),
+                ),
+              );
+            }
+            final post = postsData.posts[index];
+            return PostContainer(post: post);
+          },
+          childCount: postsData.posts.length + (_hasMore ? 1 : 0),
         ),
-      );
-    } else {
-      return _isLoading
-          ? const SliverToBoxAdapter(
-              child: Center(child: CircularProgressIndicator()),
-            )
-          : SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  final post = posts[index];
-                  return PostContainer(post: post);
-                },
-                childCount: posts.length,
-              ),
-            );
-    }
+      ),
+    );
   }
 }
