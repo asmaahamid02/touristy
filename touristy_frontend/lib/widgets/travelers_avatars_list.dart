4df7b986d25c42c8eca095bf2826a5f31a0910a3
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-14 15:00:36 +0200

    frontend: fetched data from users provider

--- touristy_frontend/lib/widgets/travelers_avatars_list.dart
+++ touristy_frontend/lib/widgets/travelers_avatars_list.dart
@@ -1,62 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import '../exceptions/http_exception.dart';
 
 import '../providers/providers.dart';
 import '../models/models.dart';
 import './widgets.dart';
 
-class TravelersAvatarsList extends StatefulWidget {
+class TravelersAvatarsList extends StatelessWidget {
   const TravelersAvatarsList({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<TravelersAvatarsList> createState() => _TravelersAvatarsListState();
-}
-
-class _TravelersAvatarsListState extends State<TravelersAvatarsList> {
-  var isInit = true;
-  var _isLoading = false;
-
-  void _showToastMessage(String message) {
-    Fluttertoast.showToast(
-        msg: message,
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0);
-  }
-
-  @override
-  void didChangeDependencies() async {
-    super.didChangeDependencies();
-    if (isInit) {
-      _isLoading = true;
-
-      try {
-        await Provider.of<TravelersUsers>(context, listen: false)
-            .fetchTravelersUsers();
-      } on HttpException catch (error) {
-        _showToastMessage(error.toString());
-      } catch (error) {
-        _showToastMessage('Could not fetch Users, try again.');
-      }
-      if (!mounted) return;
-      setState(() {
-        _isLoading = false;
-      });
-      isInit = false;
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final travelersUsersData = Provider.of<TravelersUsers>(context);
-    final travelersUsers = travelersUsersData.travelersUsers;
+    final randomUsers =
+        Provider.of<Users>(context, listen: false).getRandomUsers(30);
     return Card(
       elevation: 0,
       margin: const EdgeInsets.all(0),
@@ -70,22 +27,15 @@ class _TravelersAvatarsListState extends State<TravelersAvatarsList> {
               style: TextStyle(fontSize: 24.0),
             ),
           ),
-          _isLoading
-              ? const SizedBox(
-                  height: 100,
-                  child: Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                )
-              : Stories(
-                  storiesData: travelersUsers
-                      .map((user) => StoryData(
-                          id: user.id,
-                          name: user.fullName,
-                          url: user.profilePictureUrl,
-                          countryCode: user.countryCode))
-                      .toList(),
-                ),
+          Stories(
+            storiesData: randomUsers
+                .map((user) => StoryData(
+                    id: user.id,
+                    name: '${user.firstName} ${user.lastName}',
+                    url: user.profilePictureUrl,
+                    countryCode: user.countryCode))
+                .toList(),
+          ),
         ],
       ),
     );
