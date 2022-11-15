f97cb39ab90e924d2e534f2a07145ad8acc4e961
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-15 21:51:37 +0200

    frontend: added handleLocationPermission method

--- touristy_frontend/lib/utilities/location_handler.dart
+++ touristy_frontend/lib/utilities/location_handler.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import './utilities.dart';
 
 class LocationHandler {
   Future<String> getAddressFromLatLng(Position position) async {
@@ -30,5 +32,32 @@ class LocationHandler {
     } catch (error) {
       return error.toString();
     }
+  }
+
+  Future<bool> handleLocationPermission(BuildContext context) async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      SnakeBarCommon.show(context,
+          'Location services are disabled. Please enable the services');
+
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        SnakeBarCommon.show(context, 'Location permissions are denied');
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      SnakeBarCommon.show(context,
+          'Location permissions are permanently denied, we cannot request permissions.');
+      return false;
+    }
+    return true;
   }
 }
