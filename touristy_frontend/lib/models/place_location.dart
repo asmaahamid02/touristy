15a9709f960016cb9ae753b3396874a4f6f95af2
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-15 22:58:46 +0200

    frontend: added place location model

---
+++ touristy_frontend/lib/models/place_location.dart
@@ -0,0 +1,8 @@
+class PlaceLocation {
+  double? latitude;
+  double? longitude;
+  String? address;
+
+  PlaceLocation(
+      {required this.latitude, required this.longitude, required this.address});
+}
