e33587b9710260f0a42c3e8bb46b98afb36fe137
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-16 19:06:42 +0200

    frontend: added function to convert map into PlaceLocation object

--- touristy_frontend/lib/models/place_location.dart
+++ touristy_frontend/lib/models/place_location.dart
@@ -1,8 +1,12 @@
 class PlaceLocation {
-  double? latitude;
-  double? longitude;
-  String? address;
+  final double? latitude;
+  final double? longitude;
+  final String? address;
 
-  PlaceLocation(
-      {required this.latitude, required this.longitude, required this.address});
+  const PlaceLocation({this.latitude, this.longitude, this.address});
+
+  PlaceLocation.fromMap(Map<String, dynamic> map)
+      : latitude = map['latitude'],
+        longitude = map['longitude'],
+        address = map['address'];
 }
