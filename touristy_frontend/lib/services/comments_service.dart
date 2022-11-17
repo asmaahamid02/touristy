6982162e7da9dd36db65aa753b0bc3ba7ba4067d
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-17 13:49:04 +0200

    frontend: added addComment method service

--- touristy_frontend/lib/services/comments_service.dart
+++ touristy_frontend/lib/services/comments_service.dart
@@ -6,10 +6,10 @@ import '../models/comment.dart';
 
 class CommentService {
   static Future<List<Comment>> getCommentsByPost(
-      String token, int postId, int currenPage) async {
+      String token, int postId) async {
     try {
       final response = await http.get(
-          Uri.parse('$baseUrl/comments/post/$postId?page=$currenPage'),
+          Uri.parse('$baseUrl/comments/post/$postId'),
           headers: getHeaders(token));
       final responseData = json.decode(response.body) as Map<String, dynamic>;
 
@@ -27,6 +27,26 @@ class CommentService {
         } else {
           return [];
         }
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  //add comment
+  static Future<Comment> addComment(
+      String token, int postId, String comment) async {
+    try {
+      final response = await http.post(Uri.parse('$baseUrl/comments'),
+          headers: getHeaders(token),
+          body: json.encode({'content': comment, 'post_id': postId}));
+      final responseData = json.decode(response.body) as Map<String, dynamic>;
+
+      //check if response is has error, throw exception
+      if (response.statusCode != 200) {
+        throw HttpException(getResponseError(responseData));
+      } else {
+        return Comment.fromJson(responseData['data']);
       }
     } catch (error) {
       rethrow;
