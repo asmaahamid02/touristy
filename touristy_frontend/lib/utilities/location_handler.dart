47339712c6ac49562f4ed0b218783719367e18a9
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-18 15:23:25 +0200

    frontend: added getLocationResults function for autocomplete places

--- touristy_frontend/lib/utilities/location_handler.dart
+++ touristy_frontend/lib/utilities/location_handler.dart
@@ -1,11 +1,12 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:http/http.dart' as http;
 import './utilities.dart';
 
-const GOOGLE_API_KEY = 'AIzaSyCTh9nt09BXB492g4fj3PfAdVwhlIdJcAM';
-
 class LocationHandler {
+  static const GOOGLE_API_KEY = 'AIzaSyCTh9nt09BXB492g4fj3PfAdVwhlIdJcAM';
   static Future<Position> getCurrentPosition(BuildContext context) async {
     try {
       final hasPermission = await handleLocationPermission(context);
@@ -21,10 +22,10 @@ class LocationHandler {
   }
 
   static Future<String> getAddressFromLatLng(
-      BuildContext context, Position position) async {
+      BuildContext context, double latitude, double longitude) async {
     try {
       List<Placemark> placemarks =
-          await placemarkFromCoordinates(position.latitude, position.longitude);
+          await placemarkFromCoordinates(latitude, longitude);
       Placemark place = placemarks[0];
       String address = '';
 
@@ -78,5 +79,22 @@ class LocationHandler {
       return false;
     }
     return true;
+  }
+
+  static Future<dynamic> getLocationResults(
+      String input, String sessionToken) async {
+    String kPLACESAPIKEY = LocationHandler.GOOGLE_API_KEY;
+    String type = '(regions)';
+    String baseURL =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+    String request =
+        '$baseURL?input=$input&key=$kPLACESAPIKEY&sessiontoken=$sessionToken';
+    var response = await http.get(Uri.parse(request));
+    if (response.statusCode == 200) {
+      final res = json.decode(response.body)['predictions'];
+      return res;
+    } else {
+      throw Exception('Failed to load predictions');
+    }
   }
 }
