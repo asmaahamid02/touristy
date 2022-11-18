4410792d65db8e822e72491eed3e0a3f739a70a1
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-18 21:38:38 +0200

    backend: send fetched data

--- touristy_frontend/lib/widgets/trips_list.dart
+++ touristy_frontend/lib/widgets/trips_list.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import '../widgets/widgets.dart';
+import '../models/models.dart';
 
 class TripsList extends StatelessWidget {
   const TripsList({
     Key? key,
+    required this.trips,
   }) : super(key: key);
-
+  final List<Trip> trips;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,20 +19,12 @@ class TripsList extends StatelessWidget {
           Container(
             alignment: Alignment.centerLeft,
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Trips by travelers',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                Text(
-                  'See all',
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        color: Theme.of(context).primaryColor,
-                      ),
-                ),
-              ],
+            child: const Text(
+              'Trips by travelers',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
           Expanded(
@@ -38,17 +32,12 @@ class TripsList extends StatelessWidget {
               itemBuilder: (context, index) {
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: index == 0
-                      ? TripItem(
-                          withIcon: true,
-                          coverImageUrl: 'https://picsum.photos/${index + 100}',
-                        )
-                      : TripItem(
-                          coverImageUrl: 'https://picsum.photos/${index + 100}',
-                        ),
+                  child: TripItem(
+                    trip: trips[index],
+                  ),
                 );
               },
-              itemCount: 10,
+              itemCount: trips.length,
               padding: const EdgeInsets.symmetric(horizontal: 4.0),
               scrollDirection: Axis.horizontal,
             ),
