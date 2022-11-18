ac51f186a37e0266b8021af5f0e901b76367480e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-18 15:20:42 +0200

    frontend: moved search bar widget

---
+++ touristy_frontend/lib/widgets/search/search_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../../../utilities/utilities.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    Key? key,
+    required this.searchController,
+  }) : super(key: key);
+
+  final TextEditingController searchController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Theme.of(context).cardColor,
+      padding: const EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: double.infinity,
+        child: TextField(
+          controller: searchController,
+          decoration: InputDecoration(
+            hintText: 'Search',
+            hintStyle:
+                const TextStyle(color: AppColors.textFaded, fontSize: 14),
+            prefixIcon: const Icon(
+              Icons.search,
+              color: AppColors.textFaded,
+            ),
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(50.0),
+              ),
+              borderSide: BorderSide.none,
+            ),
+            contentPadding: const EdgeInsets.all(8.0),
+            filled: true,
+            fillColor: Theme.of(context).scaffoldBackgroundColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
