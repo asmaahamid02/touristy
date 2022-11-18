07a85021bd344f47eae0b0ddcf5765adac361f33
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-18 17:35:06 +0200

    frontend: added user id to trip model

--- touristy_frontend/lib/models/trip.dart
+++ touristy_frontend/lib/models/trip.dart
@@ -1,23 +1,30 @@
 class Trip {
-  final int id;
+  final int? id;
+  final int? userId;
   final String? title;
   final String? description;
   final String destination;
   final DateTime arrivalDate;
   final DateTime? departureDate;
+  double? latitude;
+  double? longitude;
 
   Trip({
-    required this.id,
+    this.id,
+    this.userId,
     required this.title,
     required this.description,
     required this.destination,
     required this.arrivalDate,
     required this.departureDate,
+    this.latitude,
+    this.longitude,
   });
 
   factory Trip.fromJson(Map<String, dynamic> json) {
     return Trip(
       id: json['id'],
+      userId: json['user_id'],
       title: json['title'],
       description: json['description'],
       destination:
