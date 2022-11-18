2d35716ce312c46ee87b2622fda87be0ccdc2a6c
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-18 21:40:07 +0200

    backend: added search trips service

--- touristy_frontend/lib/services/search_service.dart
+++ touristy_frontend/lib/services/search_service.dart
@@ -51,4 +51,32 @@ class SearchService {
       rethrow;
     }
   }
+
+  //search posts
+
+  //search trips
+  static Future<List<Trip>> searchTrips(String token, String query) async {
+    try {
+      final response = await http.get(
+          Uri.parse('$baseUrl/common/search/trips/$query'),
+          headers: getHeaders(token));
+      final responseData = json.decode(response.body) as Map<String, dynamic>;
+
+      //check if response is has error, throw exception
+      if (response.statusCode != 200) {
+        throw HttpException(getResponseError(responseData));
+      } else {
+        if (responseData['data'] != null &&
+            responseData['data'] != '' &&
+            responseData['data'].length > 0) {
+          final List<dynamic> tripsList = responseData['data'];
+          return tripsList.map((trip) => Trip.fromJson(trip)).toList();
+        } else {
+          return [];
+        }
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
 }
