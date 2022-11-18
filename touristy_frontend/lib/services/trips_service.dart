b862cabd198a668c6f499615ed4f0a1ac83e8815
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-18 17:35:28 +0200

    frontend: added delete trip service

--- touristy_frontend/lib/services/trips_service.dart
+++ touristy_frontend/lib/services/trips_service.dart
@@ -28,4 +28,47 @@ class TripService {
       rethrow;
     }
   }
+
+  //add trip
+  static Future<Trip> addTrip(String token, Trip trip) async {
+    try {
+      final response = await http.post(Uri.parse('$baseUrl/trips'),
+          headers: getHeaders(token),
+          body: json.encode({
+            'title': trip.title,
+            'description': trip.description,
+            'longitude': trip.longitude,
+            'latitude': trip.latitude,
+            'address': trip.destination,
+            'arrival_date': trip.arrivalDate.toIso8601String(),
+            'departure_date': trip.departureDate?.toIso8601String(),
+          }));
+      final responseData = json.decode(response.body) as Map<String, dynamic>;
+
+      //check if response is has error, throw exception
+      if (response.statusCode != 201) {
+        throw HttpException(getResponseError(responseData));
+      } else {
+        return Trip.fromJson(responseData['data']);
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  //delete trip
+  static Future<void> deleteTrip(String token, int tripId) async {
+    try {
+      final response = await http.delete(Uri.parse('$baseUrl/trips/$tripId'),
+          headers: getHeaders(token));
+      final responseData = json.decode(response.body) as Map<String, dynamic>;
+
+      //check if response is has error, throw exception
+      if (response.statusCode != 200) {
+        throw HttpException(getResponseError(responseData));
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
 }
