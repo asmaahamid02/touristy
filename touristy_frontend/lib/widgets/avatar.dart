b11ff315200e1211db7a16f4ea981692f112794e
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-20 23:55:12 +0200

    frontend: modified structure / added defaultImageProvider attribute

--- touristy_frontend/lib/widgets/avatar.dart
+++ touristy_frontend/lib/widgets/avatar.dart
@@ -2,35 +2,53 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class Avatar extends StatelessWidget {
-  Avatar({super.key, this.imageUrl, required this.radius});
+  Avatar({
+    super.key,
+    this.imageUrl,
+    required this.radius,
+    this.defaultImageProvider =
+        const AssetImage('assets/images/profile_picture.png'),
+  });
+
   Avatar.small({super.key, this.imageUrl}) : radius = 16.0;
   Avatar.medium({super.key, this.imageUrl}) : radius = 27.0;
   Avatar.large({super.key, this.imageUrl}) : radius = 44.0;
 
   String? imageUrl;
   final double radius;
+  ImageProvider? defaultImageProvider;
 
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      radius: radius,
-      backgroundColor: Theme.of(context).cardColor,
-      child: imageUrl != null
-          ? CachedNetworkImage(
-              imageUrl: imageUrl!,
-              imageBuilder: (context, imageProvider) => Container(
+        radius: radius,
+        backgroundColor: Theme.of(context).cardColor,
+        child: imageUrl != null
+            ? CachedNetworkImage(
+                imageUrl: imageUrl!,
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              )
+            : Container(
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   image: DecorationImage(
-                    image: imageProvider,
+                    image: defaultImageProvider != null
+                        ? defaultImageProvider!
+                        : const AssetImage('assets/images/profile_picture.png'),
                     fit: BoxFit.cover,
                   ),
                 ),
-              ),
-              placeholder: (context, url) => const CircularProgressIndicator(),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-            )
-          : Image.asset('assets/images/profile_picture.png'),
-    );
+              ));
   }
 }
