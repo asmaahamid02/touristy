f889d08a0062f7367f7c4dcb8550e57e22c87ff2
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-20 02:18:45 +0200

    frontend: fetched posts from user posts provider

--- touristy_frontend/lib/widgets/profile/profile_tab_bar.dart
+++ touristy_frontend/lib/widgets/profile/profile_tab_bar.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../widgets.dart';
+import '../../providers/providers.dart';
+import '../../models/models.dart';
 
 class ProfileTabBar extends StatelessWidget {
   const ProfileTabBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
+    return SliverFillRemaining(
       child: DefaultTabController(
         length: 2,
         child: Column(
@@ -28,13 +31,61 @@ class ProfileTabBar extends StatelessWidget {
             SizedBox(
               height: MediaQuery.of(context).size.height,
               child: const TabBarView(children: [
-                PostsGrid(),
+                ProfilePostsList(),
                 ProfileTripsList(),
               ]),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ProfilePostsList extends StatelessWidget {
+  const ProfilePostsList({super.key});
+
+  Future<List<Post>> _fetchPosts(BuildContext context) async {
+    final UserPosts userPosts = Provider.of<UserPosts>(context, listen: false);
+
+    final int userId = Provider.of<UserProfileProvider>(context, listen: false)
+        .userProfile
+        .id!;
+    await userPosts.fetchAndSetPosts(userId);
+    return userPosts.posts;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Post>>(
+      future: _fetchPosts(context),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          if (snapshot.error != null) {
+            return const Center(
+              child: Text('An error occurred!}'),
+            );
+          } else {
+            return Consumer<UserPosts>(
+              builder: (ctx, userPosts, child) {
+                return ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: userPosts.posts.length,
+                  itemBuilder: (ctx, index) {
+                    return PostContainer(
+                      post: userPosts.posts[index],
+                    );
+                  },
+                );
+              },
+            );
+          }
+        }
+      },
     );
   }
 }
