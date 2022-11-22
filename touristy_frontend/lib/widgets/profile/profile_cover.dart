0e8313c7c4373bd361cbcde3ad6fcd3897f91fc8
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-22 21:10:25 +0200

    frontend: checked the empty image url

--- touristy_frontend/lib/widgets/profile/profile_cover.dart
+++ touristy_frontend/lib/widgets/profile/profile_cover.dart
@@ -6,43 +6,75 @@ class ProfileCover extends StatelessWidget {
     Key? key,
     this.topPositionForUserProfile = 85,
     this.coverImageHeight = 170,
-    String? coverImageUrl,
+    this.coverImageUrl,
     this.defaultImageProvider = const AssetImage('assets/images/map.png'),
     this.onTap,
-  })  : _coverImageUrl = coverImageUrl,
-        super(key: key);
+  }) : super(key: key);
 
   final double topPositionForUserProfile;
   final double coverImageHeight;
-  final String? _coverImageUrl;
+  String? coverImageUrl;
   ImageProvider? defaultImageProvider;
   final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onTap: onTap,
-      child: Container(
-        margin: EdgeInsets.only(bottom: topPositionForUserProfile / 2),
-        height: coverImageHeight,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: _coverImageUrl != null
-                ? CachedNetworkImage(
-                    imageUrl: _coverImageUrl!,
-                    placeholder: (context, url) =>
-                        const CircularProgressIndicator(),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ) as ImageProvider
-                : defaultImageProvider != null
-                    ? defaultImageProvider!
-                    : const AssetImage('assets/images/map.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
-    );
+        behavior: HitTestBehavior.translucent,
+        onTap: onTap,
+        child: Container(
+          width: double.infinity,
+          height: coverImageHeight,
+          margin: EdgeInsets.only(bottom: topPositionForUserProfile / 2),
+          child: coverImageUrl != null
+              ? CachedNetworkImage(
+                  imageUrl: coverImageUrl!,
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                )
+              : Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    image: DecorationImage(
+                      image: defaultImageProvider != null
+                          ? defaultImageProvider!
+                          : const AssetImage('assets/images/map.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+        )
+
+        // Container(
+        //   margin: EdgeInsets.only(bottom: topPositionForUserProfile / 2),
+        //   height: coverImageHeight,
+        //   decoration: BoxDecoration(
+        //     image: DecorationImage(
+        //       image: _coverImageUrl != null
+        //           ? CachedNetworkImage(
+        //               imageUrl: _coverImageUrl!,
+        //               placeholder: (context, url) =>
+        //                   const CircularProgressIndicator(),
+        //               errorWidget: (context, url, error) =>
+        //                   const Icon(Icons.error),
+        //             ) as ImageProvider
+        //           : defaultImageProvider != null
+        //               ? defaultImageProvider!
+        //               : const AssetImage('assets/images/map.png'),
+        //       fit: BoxFit.cover,
+        //     ),
+        //   ),
+        // ),
+        );
   }
 }
