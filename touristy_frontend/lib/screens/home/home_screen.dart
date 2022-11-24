820d6db2ac6c2c923d7cdb2be095b2e1c49df433
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-24 23:37:18 +0200

    frontend: prevented top TabBar from scrolling

--- touristy_frontend/lib/screens/home/home_screen.dart
+++ touristy_frontend/lib/screens/home/home_screen.dart
@@ -72,6 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         body: const TabBarView(
+          physics: NeverScrollableScrollPhysics(),
           children: [
             ForYouPage(),
             FollowingPage(),
