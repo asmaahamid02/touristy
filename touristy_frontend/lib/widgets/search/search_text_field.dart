849989c3d2af9bcc2ee01600b956899fd65ecaf1
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-24 23:46:34 +0200

    frontend: changed attributes

--- touristy_frontend/lib/widgets/search/search_text_field.dart
+++ touristy_frontend/lib/widgets/search/search_text_field.dart
@@ -3,29 +3,32 @@ import 'package:flutter/material.dart';
 class SearchTextField extends StatelessWidget {
   SearchTextField({
     Key? key,
-    required this.focusNode,
+    this.focusNode,
     required this.controller,
     required this.labelText,
     required this.onTap,
     required this.readOnly,
+    this.border,
     this.validator,
     this.autoFocus = false,
   }) : super(key: key);
 
-  final FocusNode focusNode;
+  final FocusNode? focusNode;
   final TextEditingController controller;
   final String labelText;
   final Function()? onTap;
   String? Function(String?)? validator;
   final bool readOnly;
   final bool autoFocus;
+  final InputBorder? border;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(
-        border: const UnderlineInputBorder(),
-        labelText: labelText,
+        border: border ?? const UnderlineInputBorder(),
+        // labelText: labelText,
+        hintText: labelText,
         prefixIcon: const Icon(Icons.location_on_outlined),
       ),
       readOnly: readOnly,
