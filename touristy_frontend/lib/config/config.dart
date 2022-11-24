ed128868bfc1344e7068aa64cbb083730c749711
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-24 23:45:46 +0200

    frontend: added global config file

---
+++ touristy_frontend/lib/config/config.dart
@@ -0,0 +1,8 @@
+library config.globals;
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../providers/providers.dart';
+
+MyTheme currentTheme = MyTheme();
+SharedPreferences? prefs;
