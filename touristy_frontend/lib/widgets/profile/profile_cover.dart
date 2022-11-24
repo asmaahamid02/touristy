f75d2ebc4e98350e95a4a31b1e250cee6bd2d821
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-24 23:49:30 +0200

    frontend: modified the structure

--- touristy_frontend/lib/widgets/profile/profile_cover.dart
+++ touristy_frontend/lib/widgets/profile/profile_cover.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import '../../utilities/utilities.dart';
 
 class ProfileCover extends StatelessWidget {
   ProfileCover({
@@ -20,61 +21,41 @@ class ProfileCover extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: onTap,
-        child: Container(
-          width: double.infinity,
-          height: coverImageHeight,
-          margin: EdgeInsets.only(bottom: topPositionForUserProfile / 2),
-          child: coverImageUrl != null
-              ? CachedNetworkImage(
-                  imageUrl: coverImageUrl!,
-                  imageBuilder: (context, imageProvider) => Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      image: DecorationImage(
-                        image: imageProvider,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  placeholder: (context, url) =>
-                      const CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                )
-              : Container(
+      behavior: HitTestBehavior.translucent,
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: coverImageHeight,
+        margin: EdgeInsets.only(bottom: topPositionForUserProfile / 2),
+        child: coverImageUrl != null
+            ? CachedNetworkImage(
+                imageUrl: coverImageUrl!,
+                imageBuilder: (context, imageProvider) => Container(
                   decoration: BoxDecoration(
                     shape: BoxShape.rectangle,
                     image: DecorationImage(
-                      image: defaultImageProvider != null
-                          ? defaultImageProvider!
-                          : const AssetImage('assets/images/map.png'),
+                      image: imageProvider,
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
-        )
-
-        // Container(
-        //   margin: EdgeInsets.only(bottom: topPositionForUserProfile / 2),
-        //   height: coverImageHeight,
-        //   decoration: BoxDecoration(
-        //     image: DecorationImage(
-        //       image: _coverImageUrl != null
-        //           ? CachedNetworkImage(
-        //               imageUrl: _coverImageUrl!,
-        //               placeholder: (context, url) =>
-        //                   const CircularProgressIndicator(),
-        //               errorWidget: (context, url, error) =>
-        //                   const Icon(Icons.error),
-        //             ) as ImageProvider
-        //           : defaultImageProvider != null
-        //               ? defaultImageProvider!
-        //               : const AssetImage('assets/images/map.png'),
-        //       fit: BoxFit.cover,
-        //     ),
-        //   ),
-        // ),
-        );
+                placeholder: (context, url) => Container(
+                  color: AppColors.backgroundLightGrey,
+                ),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              )
+            : Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                    image: defaultImageProvider != null
+                        ? defaultImageProvider!
+                        : const AssetImage('assets/images/map.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+      ),
+    );
   }
 }
