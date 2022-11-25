5032dda2b92dae7e1a2ff1605113d2cdff153221
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-25 21:23:13 +0200

    frontend: added onTap functionality

--- touristy_frontend/lib/screens/home/map_page.dart
+++ touristy_frontend/lib/screens/home/map_page.dart
@@ -3,7 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:custom_map_markers/custom_map_markers.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import '../../widgets/widgets.dart';
 import '../../screens/screens.dart';
 import '../../models/models.dart';
 import '../../utilities/utilities.dart';
@@ -80,6 +82,7 @@ class _MapPageState extends State<MapPage> {
                 marker: Marker(
                   markerId: MarkerId('id-${id.toString()}'),
                   position: LatLng(element.latitude!, element.longitude!),
+                  onTap: () => _showTripsBottomSheet(context, element.ids!),
                 ),
                 child: _customMarker(numberOfTrips: element.count!),
               ),
@@ -93,6 +96,25 @@ class _MapPageState extends State<MapPage> {
       }
 
       _isInit = false;
+    }
+  }
+
+  Future<void> _showTripsBottomSheet(
+      BuildContext context, List<int> ids) async {
+    try {
+      await Provider.of<Trips>(context, listen: false).fetchTripsByIds(ids);
+
+      if (!mounted) return;
+      final trips = Provider.of<Trips>(context, listen: false).locationsTrips;
+      final users = Provider.of<Users>(context, listen: false).users;
+
+      ModalBottomSheetCommon.show(
+        context,
+        _TripCard(trips: trips, users: users),
+        height: 300,
+      );
+    } catch (error) {
+      debugPrint('error : $error');
     }
   }
 
@@ -164,6 +186,66 @@ class _MapPageState extends State<MapPage> {
           ),
         )
       ],
+    );
+  }
+}
+
+class _TripCard extends StatelessWidget {
+  const _TripCard({
+    Key? key,
+    required this.trips,
+    required this.users,
+  }) : super(key: key);
+
+  final List<Trip> trips;
+  final List<User> users;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: ListView.separated(
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: trips.length,
+        itemBuilder: (ctx, index) {
+          final trip = trips[index];
+          final user = users.firstWhere((element) => element.id == trip.userId);
+          final username = '${user.firstName} ${user.lastName}';
+
+          String date = trip.departureDate != null
+              ? DateFormat.yMMMd().format(trip.departureDate!)
+              : '';
+          if (trip.arrivalDate != null) {
+            if (date.isNotEmpty) date += ' - ';
+            date += DateFormat.yMMMd().format(trip.arrivalDate!);
+          }
+          return ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+            leading: Avatar.small(imageUrl: user.profilePictureUrl),
+            title: Text(username),
+            subtitle: Text(
+              date,
+              style: const TextStyle(fontSize: 12, color: AppColors.textFaded),
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.message_outlined),
+              onPressed: () => Navigator.of(context).pushNamed(
+                ChatScreen.routeName,
+                arguments: MessageData(
+                  senderId: user.id,
+                  senderName: username,
+                  profilePicture: user.profilePictureUrl,
+                ),
+              ),
+            ),
+            onTap: () => Navigator.of(context)
+                .pushNamed(ProfileScreen.routeName, arguments: {
+              'userId': user.id,
+              'username': username,
+            }),
+          );
+        },
+      ),
     );
   }
 }
