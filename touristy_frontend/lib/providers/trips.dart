0f55dd51e1318b0abafef3e59f594ba452b1bda2
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-11-25 21:26:42 +0200

    frontend: added locationsTrips list

--- touristy_frontend/lib/providers/trips.dart
+++ touristy_frontend/lib/providers/trips.dart
@@ -6,6 +6,8 @@ import './providers.dart';
 
 class Trips with ChangeNotifier {
   List<Trip> _trips = [];
+  List<Trip> _locationsTrips = [];
+
   String? authToken;
   DateTime? _lastUpdated;
   int? lastUpdatedUserId;
@@ -22,14 +24,17 @@ class Trips with ChangeNotifier {
 
   List<Trip> get trips => _trips;
 
-  Future<void> fetchTrips(int userId) async {
+  List<Trip> get locationsTrips => _locationsTrips;
+
+  Future<void> fetchTripsByUser(int userId) async {
     if (lastUpdatedUserId == userId &&
         _lastUpdated != null &&
         DateTime.now().difference(_lastUpdated!).inMinutes < 1) {
       return;
     }
     try {
-      final List<Trip> trips = await TripService.getTrips(authToken!, userId);
+      final List<Trip> trips =
+          await TripsService.getTripsByUser(authToken!, userId);
       _trips = trips;
       _lastUpdated = DateTime.now();
       lastUpdatedUserId = userId;
@@ -42,7 +47,7 @@ class Trips with ChangeNotifier {
   //add trip
   Future<void> addTrip(Trip trip) async {
     try {
-      final Trip newTrip = await TripService.addTrip(authToken!, trip);
+      final Trip newTrip = await TripsService.addTrip(authToken!, trip);
       _trips.insert(0, newTrip);
       notifyListeners();
     } catch (error) {
@@ -61,11 +66,22 @@ class Trips with ChangeNotifier {
     notifyListeners();
 
     try {
-      await TripService.deleteTrip(authToken!, tripId);
+      await TripsService.deleteTrip(authToken!, tripId);
       _trips.removeWhere((trip) => trip.id == tripId);
     } catch (error) {
       _trips.insert(deletedTripIndex, deletedTrip);
       notifyListeners();
+      rethrow;
+    }
+  }
+
+  Future<void> fetchTripsByIds(List<int> ids) async {
+    try {
+      final List<Trip> trips =
+          await TripsService.getTripsByIds(authToken!, ids);
+      _locationsTrips = trips;
+      notifyListeners();
+    } catch (error) {
       rethrow;
     }
   }
